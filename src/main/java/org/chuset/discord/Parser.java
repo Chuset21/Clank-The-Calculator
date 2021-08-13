4767009a110e71cd73a87413efdedeb674f4a686
Miguel <miguelarrieta02@gmail.com>
2021-08-13 23:22:25 +0100

    Added Parser class to parse a mathematical expression

---
+++ src/main/java/org/chuset/discord/Parser.java
@@ -0,0 +1,111 @@
+package org.chuset.discord;
+
+public class Parser {
+    private final String str;
+    private int pos;
+    private int ch;
+
+    public Parser(final String str) {
+        this.str = str;
+        pos = -1;
+    }
+
+    private void nextChar() {
+        ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+    }
+
+    private boolean eat(final char charToEat) {
+        while (ch == ' ') {
+            nextChar();
+        }
+        if (ch == charToEat) {
+            nextChar();
+            return true;
+        }
+        return false;
+    }
+
+    public double parse() {
+        nextChar();
+        final double x = parseExpression();
+        if (pos < str.length()) {
+            throw new RuntimeException("Unexpected: " + (char) ch);
+        }
+        return x;
+    }
+
+    // Grammar:
+    // expression = term | expression `+` term | expression `-` term
+    // term = factor | term `*` factor | term `/` factor
+    // factor = `+` factor | `-` factor | `(` expression `)`
+    //        | number | functionName factor | factor `^` factor
+
+    private double parseExpression() {
+        double x = parseTerm();
+        while (true) {
+            if (eat('+')) {
+                x += parseTerm(); // addition
+            } else if (eat('-')) {
+                x -= parseTerm(); // subtraction
+            } else {
+                return x;
+            }
+        }
+    }
+
+    private double parseTerm() {
+        double x = parseFactor();
+        while (true) {
+            if (eat('*')) {
+                x *= parseFactor(); // multiplication
+            } else if (eat('/')) {
+                x /= parseFactor(); // division
+            } else {
+                return x;
+            }
+        }
+    }
+
+    private double parseFactor() {
+        if (eat('+')) {
+            return parseFactor(); // unary plus
+        }
+
+        if (eat('-')) {
+            return -parseFactor(); // unary minus
+        }
+
+        double x;
+        final int startPos = pos;
+        if (eat('(')) { // parentheses
+            x = parseExpression();
+            eat(')');
+        } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+            while ((ch >= '0' && ch <= '9') || ch == '.') {
+                nextChar();
+            }
+            x = Double.parseDouble(str.substring(startPos, pos));
+        } else if (ch >= 'a' && ch <= 'z') { // functions
+            while (ch >= 'a' && ch <= 'z') {
+                nextChar();
+            }
+            final String func = str.substring(startPos, pos);
+            x = parseFactor();
+            x = switch (func) {
+                case "sqrt" -> Math.sqrt(x);
+                case "sin" -> Math.sin(Math.toRadians(x));
+                case "cos" -> Math.cos(Math.toRadians(x));
+                case "tan" -> Math.tan(Math.toRadians(x));
+                default -> throw new RuntimeException("Unknown function: " + func);
+            };
+        } else {
+            throw new RuntimeException("Unexpected: " + (char) ch);
+        }
+
+        if (eat('^')) {
+            x = Math.pow(x, parseFactor()); // exponentiation
+        }
+
+        return x;
+    }
+}
