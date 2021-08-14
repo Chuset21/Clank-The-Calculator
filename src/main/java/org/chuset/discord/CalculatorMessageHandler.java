155538afe2ee97c1648ebb33ba25391910f7692e
Miguel <miguelarrieta02@gmail.com>
2021-08-15 00:55:28 +0100

    Changed formatter method to doubleFormatterToString and updated it to make exponents more readable for the user

--- src/main/java/org/chuset/discord/CalculatorMessageHandler.java
+++ src/main/java/org/chuset/discord/CalculatorMessageHandler.java
@@ -7,15 +7,15 @@ import java.util.Locale;
 public class CalculatorMessageHandler implements MessageTextHandler {
     @Override
     public String handleMessage(final String message) {
-        return decimalFormatter(evaluateExpression(message.toLowerCase(Locale.ROOT)));
+        return doubleFormatterToString(evaluateExpression(message.toLowerCase(Locale.ROOT)));
     }
 
-    public static String decimalFormatter(final double number) {
+    public static String doubleFormatterToString(final double number) {
         final long longNum = (long) number;
         if (number == longNum) {
             return String.format("%d", longNum);
         } else {
-            return String.format("%s", number);
+            return String.format("%s", number).replace("E", " * 10^");
         }
     }
 
