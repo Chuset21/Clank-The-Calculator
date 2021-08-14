dde7bf45740065c044e2384d73782bbcb4800669
Miguel <miguelarrieta02@gmail.com>
2021-08-15 00:29:08 +0100

    Added fibonacci method

---
+++ src/main/java/org/chuset/discord/util/Fib.java
@@ -0,0 +1,24 @@
+package org.chuset.discord.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fib {
+    private static final Map<Long, Long> CACHE = new HashMap<>();
+
+    public static long fibonacci(final long n) {
+        return fibonacciHelper(n, 1, 0);
+    }
+
+    private static long fibonacciHelper(final long n, final long val, final long prev) { // O(n)
+        if (CACHE.containsKey(n)) {
+            return CACHE.get(n);
+        } else if (n == 0) {
+            return prev;
+        } else if (n == 1) {
+            return val;
+        }
+        CACHE.putIfAbsent(n, fibonacciHelper(n - 1, val + prev, val));
+        return CACHE.get(n);
+    }
+}
