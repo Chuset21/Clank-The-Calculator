bcab404b3142914b6991c5c276b1a3ac51cd53b7
Miguel <miguelarrieta02@gmail.com>
2021-08-14 01:13:23 +0100

    Made jdaToken final

--- src/main/java/org/chuset/discord/BotMain.java
+++ src/main/java/org/chuset/discord/BotMain.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class BotMain {
 
     public static void main(String[] args) throws Exception {
-        String jdaToken = System.getenv("JDA_TOKEN");
+        final String jdaToken = System.getenv("JDA_TOKEN");
         JDA jda = JDABuilder.createDefault(jdaToken).build();
         jda.addEventListener(new MessageListener(jda.getSelfUser(), new CalculatorMessageHandler()));
     }
