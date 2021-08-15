2083f7f8bfe260e16cce38d7634f83131d8e16b8
Miguel <miguelarrieta02@gmail.com>
2021-08-15 01:54:47 +0100

    Fixed error in Fib.fibonacci

--- src/main/java/org/chuset/discord/util/Fib.java
+++ src/main/java/org/chuset/discord/util/Fib.java
@@ -7,18 +7,14 @@ public class Fib {
     private static final Map<Long, Long> CACHE = new HashMap<>();
 
     public static long fibonacci(final long n) {
-        return fibonacciHelper(n, 1, 0);
-    }
-
-    private static long fibonacciHelper(final long n, final long val, final long prev) { // O(n)
-        if (CACHE.containsKey(n)) {
-            return CACHE.get(n);
-        } else if (n == 0) {
-            return prev;
+        if (n == 0) {
+            return 0;
         } else if (n == 1) {
-            return val;
+            return 1;
+        } else if (CACHE.containsKey(n)) {
+            return CACHE.get(n);
         }
-        CACHE.put(n, fibonacciHelper(n - 1, val + prev, val));
+        CACHE.putIfAbsent(n, fibonacci(n - 1) + fibonacci(n - 2));
         return CACHE.get(n);
     }
 }
