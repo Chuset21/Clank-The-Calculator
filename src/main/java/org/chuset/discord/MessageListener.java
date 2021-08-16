e075f540eb1c77a0a1e556197e9cd7dca8d81d44
Miguel <miguelarrieta02@gmail.com>
2021-08-16 22:09:53 +0100

    Removed unnecessary import

--- src/main/java/org/chuset/discord/MessageListener.java
+++ src/main/java/org/chuset/discord/MessageListener.java
@@ -1,7 +1,6 @@
 package org.chuset.discord;
 
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
