24d11c2f4ecdd2a7d3fbc12ed4bef5a579c12cb4
Miguel <miguelarrieta02@gmail.com>
2021-08-16 21:24:12 +0100

    Made jda variable final in BotMain

--- src/main/java/org/chuset/discord/BotMain.java
+++ src/main/java/org/chuset/discord/BotMain.java
@@ -7,7 +7,7 @@ public class BotMain {
 
     public static void main(String[] args) throws Exception {
         final String jdaToken = System.getenv("JDA_TOKEN");
-        JDA jda = JDABuilder.createDefault(jdaToken).build();
+        final JDA jda = JDABuilder.createDefault(jdaToken).build();
         jda.addEventListener(new MessageListener(jda.getSelfUser(), new CalculatorMessageHandler()));
     }
 }
