000016de5364529fd1d2648acdb7260072f40a85
Miguel <miguelarrieta02@gmail.com>
2021-08-18 13:08:00 +0100

    Made a small improvement to Log.log

--- src/main/java/org/chuset/discord/util/Log.java
+++ src/main/java/org/chuset/discord/util/Log.java
@@ -2,6 +2,9 @@ package org.chuset.discord.util;
 
 public class Log {
     public static double log(final double a, final double b) {
+        if (b == 10) {
+            return Math.log10(a);
+        }
         return Math.log(a) / Math.log(b);
     }
 }
