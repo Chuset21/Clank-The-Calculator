826c6b604bdb89492371b5a64301abcb624a803a
Miguel <miguelarrieta02@gmail.com>
2021-08-30 15:22:31 +0100

    Trolling commenced

--- src/main/java/org/chuset/discord/MessageListener.java
+++ src/main/java/org/chuset/discord/MessageListener.java
@@ -1,11 +1,12 @@
 package org.chuset.discord;
 
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.concurrent.CancellationException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MessageListener extends ListenerAdapter {
 
@@ -23,8 +24,21 @@ public class MessageListener extends ListenerAdapter {
             final Message message = event.getMessage();
             try {
                 if (message.isMentioned(selfUser)) {
-                    message.reply(handler.handleMessage(
-                            message.getContentRaw().replaceAll("<@.*>", "").trim())).queue();
+                    final String rawMessage = message.getContentRaw().replaceFirst("<@.\\d+>", "").trim();
+                    final Matcher m = Pattern.compile("^(.*)\\s+(\\d+)$").matcher(rawMessage);
+                    if (!m.find() || m.groupCount() != 2) {
+                        throw new RuntimeException("No Matches");
+                    }
+
+                    final TextChannel channel = event.getTextChannel();
+                    channel.sendMessage("Prepare yourself\nYou will enjoy this.").complete().delete().complete();
+
+                    final int limit = Math.max(Math.min(Integer.parseInt(m.group(2)), 100), 1);
+                    final String text = m.group(1);
+                    for (int i = 0; i < limit; i++) {
+                        channel.sendMessage(text).complete().delete().complete();
+                    }
+                    channel.sendMessage("Trolling complete.").complete().delete().complete();
                 }
             } catch (net.dv8tion.jda.api.exceptions.InsufficientPermissionException e) {
                 e.printStackTrace();
