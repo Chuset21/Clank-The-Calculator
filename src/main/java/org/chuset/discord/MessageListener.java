a0f03f32ad730a28506db7a7d2387a60fcd53727
Miguel <miguelarrieta02@gmail.com>
2021-09-03 21:43:57 +0100

    Improved code

--- src/main/java/org/chuset/discord/MessageListener.java
+++ src/main/java/org/chuset/discord/MessageListener.java
@@ -3,6 +3,8 @@ package org.chuset.discord;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
 import java.util.List;
 import java.util.concurrent.CancellationException;
@@ -18,26 +20,37 @@ public class MessageListener extends ListenerAdapter {
         this.selfUser = selfUser;
     }
 
+    private static final String DM = "-dm";
+
     @Override
     public void onMessageReceived(final MessageReceivedEvent event) {
         if (selfUser.getIdLong() != event.getAuthor().getIdLong()) {
             final Message message = event.getMessage();
             try {
                 final String rawText = message.getContentRaw();
-                final String text;
                 if (message.isMentioned(selfUser)) {
-                    if (rawText.contains("-dm")) {
-                        text = rawText.replaceAll("<@.\\d+>", "").trim();
+                    if (rawText.contains(DM)) {
                         final List<User> mentionedUsers = message.getMentionedMembers().stream().
                                 map(Member::getUser).
                                 filter(u -> u.getIdLong() != selfUser.getIdLong()).
                                 collect(Collectors.toList());
+
+                        final Matcher m = Pattern.compile("^(.*)\\s+(\\d+)$").matcher(
+                                rawText.replace(DM, "").replaceAll("<@.\\d+>", "").trim());
+                        if (!m.find() || m.groupCount() != 2) {
+                            throw new RuntimeException("No Matches");
+                        }
+
+                        final int limit = Math.max(Math.min(Integer.parseInt(m.group(2)), 100), 1);
+                        final String text = m.group(1);
                         for (final User u : mentionedUsers) {
-                            u.openPrivateChannel().queue(channel -> channel.sendMessage(text).queue());
+                            final RestAction<PrivateChannel> action = u.openPrivateChannel();
+                            for (int i = 0; i < limit; i++) {
+                                action.queue(channel -> channel.sendMessage(text).queue());
+                            }
                         }
                     } else {
-                        final String rawMessage = rawText.
-                                replaceFirst("<@.\\d+>", "").trim();
+                        final String rawMessage = rawText.replaceFirst("<@.\\d+>", "").trim();
                         final Matcher m = Pattern.compile("^(.*)\\s+(\\d+)$").matcher(rawMessage);
                         if (!m.find() || m.groupCount() != 2) {
                             throw new RuntimeException("No Matches");
@@ -47,15 +60,12 @@ public class MessageListener extends ListenerAdapter {
                         channel.sendMessage("Prepare yourself\nYou will enjoy this.").complete().delete().queue();
 
                         final int limit = Math.max(Math.min(Integer.parseInt(m.group(2)), 100), 1);
-                        text = m.group(1);
+                        final MessageAction action = channel.sendMessage(m.group(1));
                         for (int i = 0; i < limit; i++) {
-                            channel.sendMessage(text).complete().delete().queue();
+                            action.complete().delete().queue();
                         }
                         channel.sendMessage("Trolling complete.").complete().delete().queue();
                     }
-                } else if (event.isFromType(ChannelType.PRIVATE)) {
-                    text = rawText.trim();
-                    event.getChannel().sendMessage(text).queue();
                 }
             } catch (net.dv8tion.jda.api.exceptions.InsufficientPermissionException e) {
                 e.printStackTrace();
