6c9e35cc6e8ce1196111239f01ddc7416a0a0e56
Miguel <miguelarrieta02@gmail.com>
2021-09-03 21:30:58 +0100

    Merged dm and trolling

--- src/main/java/org/chuset/discord/MessageListener.java
+++ src/main/java/org/chuset/discord/MessageListener.java
@@ -4,9 +4,11 @@ import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class MessageListener extends ListenerAdapter {
 
@@ -21,22 +23,39 @@ public class MessageListener extends ListenerAdapter {
         if (selfUser.getIdLong() != event.getAuthor().getIdLong()) {
             final Message message = event.getMessage();
             try {
+                final String rawText = message.getContentRaw();
+                final String text;
                 if (message.isMentioned(selfUser)) {
-                    final String rawMessage = message.getContentRaw().replaceFirst("<@.\\d+>", "").trim();
-                    final Matcher m = Pattern.compile("^(.*)\\s+(\\d+)$").matcher(rawMessage);
-                    if (!m.find() || m.groupCount() != 2) {
-                        throw new RuntimeException("No Matches");
-                    }
+                    if (rawText.contains("-dm")) {
+                        text = rawText.replaceAll("<@.\\d+>", "").trim();
+                        final List<User> mentionedUsers = message.getMentionedMembers().stream().
+                                map(Member::getUser).
+                                filter(u -> u.getIdLong() != selfUser.getIdLong()).
+                                collect(Collectors.toList());
+                        for (final User u : mentionedUsers) {
+                            u.openPrivateChannel().queue(channel -> channel.sendMessage(text).queue());
+                        }
+                    } else {
+                        final String rawMessage = rawText.
+                                replaceFirst("<@.\\d+>", "").trim();
+                        final Matcher m = Pattern.compile("^(.*)\\s+(\\d+)$").matcher(rawMessage);
+                        if (!m.find() || m.groupCount() != 2) {
+                            throw new RuntimeException("No Matches");
+                        }
 
-                    final TextChannel channel = event.getTextChannel();
-                    channel.sendMessage("Prepare yourself\nYou will enjoy this.").complete().delete().queue();
+                        final TextChannel channel = event.getTextChannel();
+                        channel.sendMessage("Prepare yourself\nYou will enjoy this.").complete().delete().queue();
 
-                    final int limit = Math.max(Math.min(Integer.parseInt(m.group(2)), 100), 1);
-                    final String text = m.group(1);
-                    for (int i = 0; i < limit; i++) {
-                        channel.sendMessage(text).complete().delete().queue();
+                        final int limit = Math.max(Math.min(Integer.parseInt(m.group(2)), 100), 1);
+                        text = m.group(1);
+                        for (int i = 0; i < limit; i++) {
+                            channel.sendMessage(text).complete().delete().queue();
+                        }
+                        channel.sendMessage("Trolling complete.").complete().delete().queue();
                     }
-                    channel.sendMessage("Trolling complete.").complete().delete().queue();
+                } else if (event.isFromType(ChannelType.PRIVATE)) {
+                    text = rawText.trim();
+                    event.getChannel().sendMessage(text).queue();
                 }
             } catch (net.dv8tion.jda.api.exceptions.InsufficientPermissionException e) {
                 e.printStackTrace();
